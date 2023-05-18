ed3bba575aefe0453e3f98ebd3941c8f4f64232b
akash sarkar <shoaibsarkarakash019@gmail.com>
2023-05-18 13:13:00 +0600

    another preactice

---
+++ practice1.js
@@ -0,0 +1,52 @@
+var i=0;
+// print ajke amr mon valo nai
+/*for( i=1; i<40 ; i++){
+   console.log("ajke amr mon valo nai");
+}*/
+// print number between 58 to 98
+// for (i=59; i<98 ; i++){
+//    console.log(i);
+// }
+// all the even number between 412 to 456
+// for(i=412; i<=456; i=i+2){
+//    console.log(i);
+// }
+// for(i=581; i<=623; i=i+2){
+//    console.log(i);
+   
+// }
+// var names=["oppo","reamle","vivo","iPhone","samsung","poco","redmi"];
+// while(i<names.length){
+//    var name=names[i];
+//    console.log(name);
+//    i++
+// }
+// for(i=30; i<=86 ; i++){
+//    console.log(i);
+//    if(i==50){
+//       break;
+//    }
+// }
+//  var booksPrice=[50,60,70,89,400,200,150];
+
+// for(i=0; i<booksPrice.length; i++){
+//    var book=booksPrice[i];
+//    console.log(book);
+//     if(booksPrice>100){
+      
+//       break;
+      
+//       }
+   
+// }
+// var booksPrice=[50,60,70,89,700,400,200,150];
+// while(i<booksPrice.length){
+//    var book=booksPrice[i];
+//    console.log(book);
+//      i++
+//    if(book>200){
+      
+//       break;
+
+//    }
+// }
